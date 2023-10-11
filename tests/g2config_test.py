dbb41421362fc4d8b78fda579e6300c407c29e68
docktermj <michael@dockter.com>
2023-10-11 17:46:01 -0400

    #3 Add skeleton files

--- tests/g2config_test.py
+++ tests/g2config_test.py
@@ -7,3 +7,26 @@
 # Import from standard library. https://docs.python.org/3/library/
 
 import unittest
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        result = 1
+        self.assertEqual(result, 6)
+
+    def test_list_int2(self):
+        result = 2
+        self.assertEqual(result, 6)
+
+
+    def test_list_int3(self):
+        result = 6
+        self.assertEqual(result, 6)
+
+    def test_list_int4(self):
+        result = 6
+        self.assertEqual(result, 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
