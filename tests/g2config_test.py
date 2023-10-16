13096e82512d54c2558f6adfc21933247ae019a4
docktermj <michael@dockter.com>
2023-10-16 09:50:44 -0400

    #3 fix unittest

--- tests/g2config_test.py
+++ tests/g2config_test.py
@@ -10,20 +10,8 @@ import unittest
 
 
 class TestSum(unittest.TestCase):
+
     def test_list_int(self):
-        result = 1
-        self.assertEqual(result, 6)
-
-    def test_list_int2(self):
-        result = 2
-        self.assertEqual(result, 6)
-
-
-    def test_list_int3(self):
-        result = 6
-        self.assertEqual(result, 6)
-
-    def test_list_int4(self):
         result = 6
         self.assertEqual(result, 6)
 
