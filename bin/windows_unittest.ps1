eed537c7310c6a2a791c227dca108c43e2c13143
docktermj <michael@dockter.com>
2023-10-17 17:44:55 -0400

    #3 Work on Windows DLL - 9

--- bin/windows_unittest.ps1
+++
@@ -1,8 +0,0 @@
-
-# echo "C:\Program Files\Senzing\g2\lib" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
-echo "C:\PROGRA~1\Senzing\g2\lib" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
-$Env:Path
-$Env:PATH += ";C:\PROGRA~1\Senzing\g2\lib"
-$Env:Path
-$Env:LD_LIBRARY_PATH += ";C:\PROGRA~1\Senzing\g2\lib"
-python3 -m unittest tests/g2config_test.py tests/g2configmgr_test.py tests/g2diagnostic_test.py tests/g2engine_test.py tests/g2engineflags_test.py tests/g2exception_test.py tests/g2hasher_test.py tests/g2product_test.py
