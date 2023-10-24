ad1c16c0cfe23c5efa52e92f829b274125d70556
docktermj <michael@dockter.com>
2023-10-24 18:20:49 -0400

    #3 Multiplatform support - 2

---
+++ tests/g2testhelper.py
@@ -0,0 +1,36 @@
+import json
+from sys import platform
+
+
+def get_test_engine_configuration_json() -> str:
+    if platform == "linux" or platform == "linux2":
+        test_engine_configuration = {
+            "PIPELINE": {
+                "CONFIGPATH": "/etc/opt/senzing",
+                "RESOURCEPATH": "/opt/senzing/g2/resources",
+                "SUPPORTPATH": "/opt/senzing/data",
+            },
+            "SQL": {"CONNECTION": "sqlite3://na:na@/tmp/sqlite/G2C.db"},
+        }
+    elif platform == "darwin":
+        test_engine_configuration = {
+            "PIPELINE": {
+                "CONFIGPATH": "/opt/senzing/g2/etc",
+                "RESOURCEPATH": "/opt/senzing/g2/resources",
+                "SUPPORTPATH": "/opt/senzing/g2/data",
+            },
+            "SQL": {"CONNECTION": "sqlite3://na:na@/tmp/sqlite/G2C.db"},
+        }
+    elif platform == "win32":
+        test_engine_configuration = {
+            "PIPELINE": {
+                "CONFIGPATH": "C:\\Program Files\\Senzing\\g2\\etc",
+                "RESOURCEPATH": "C:\\Program Files\\Senzing\\g2\\resources",
+                "SUPPORTPATH": "C:\\Program Files\\Senzing\\g2\\data",
+            },
+            "SQL": {"CONNECTION": "sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db"},
+        }
+    else:
+        test_engine_configuration = {}
+
+    return json.dumps(test_engine_configuration)
