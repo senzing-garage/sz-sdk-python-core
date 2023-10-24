ca5d2debd2cf676ec88a283b854aac975c02a3c8
docktermj <michael@dockter.com>
2023-10-24 11:11:05 -0400

    #3 Fix isort issues - 5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,31 +6,32 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import sys
 import os
-project = 'g2-sdk-python-next'
-copyright = '2023, Senzing'
-author = 'senzing'
+import sys
+
+project = "g2-sdk-python-next"
+copyright = "2023, Senzing"
+author = "senzing"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 
 # -- Customization -----------------------------------------------------------
 
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 extensions = [
     "autodocsumm",  # to generate tables of functions, attributes, methods, etc.
@@ -42,10 +43,8 @@ extensions = [
     "sphinx.ext.viewcode",  # to allow vieing the source code in the web page
 ]
 
-exclude_patterns = [
-    '*.py'
-]
+exclude_patterns = ["*.py"]
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 # autodoc_inherit_docstrings = False  # don't include docstrings from the parent class
 # autodoc_typehints = "description"   # Show types only in descriptions, not in signatures
