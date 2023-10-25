2cd999fdc32325cab39e64ed06d698e3f1d92e29
Ant <ant@senzing.com>
2023-10-25 18:38:03 -0400

    #3 - Fixing code quality

--- tests/g2diagnostic_test.py
+++ tests/g2diagnostic_test.py
@@ -14,12 +14,12 @@ import pytest
 from senzing import g2diagnostic
 
 ENGINE_MODULE_NAME = "Example"
-# ENGINE_CONFIGURATION_JSON = str(
-#    '{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/g2/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION":"sqlite3://na:na@/tmp/sqlite/G2C.db"}}'
-# )
 ENGINE_CONFIGURATION_JSON = str(
-    '{"PIPELINE": {"SUPPORTPATH": "/home/ant/senzprojs/3.8.0.23292/data", "CONFIGPATH": "/home/ant/senzprojs/3.8.0.23292/etc", "RESOURCEPATH": "/home/ant/senzprojs/3.8.0.23292/resources"}, "SQL": {"CONNECTION": "postgresql://senzing:password@ant76:5432:g2"}}'
+    '{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/g2/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION":"sqlite3://na:na@/tmp/sqlite/G2C.db"}}'
 )
+# ENGINE_CONFIGURATION_JSON = str(
+#     '{"PIPELINE": {"SUPPORTPATH": "/home/ant/senzprojs/3.8.0.23292/data", "CONFIGPATH": "/home/ant/senzprojs/3.8.0.23292/etc", "RESOURCEPATH": "/home/ant/senzprojs/3.8.0.23292/resources"}, "SQL": {"CONNECTION": "postgresql://senzing:password@ant76:5432:g2"}}'
+# )
 ENGINE_VERBOSE_LOGGING = 0
 
 
