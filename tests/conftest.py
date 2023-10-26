513d9bb3effe1d5a3bc24d70f1a265082b186c41
Ant <ant@senzing.com>
2023-10-26 15:42:40 -0400

    #3 - Fixing

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ def engine_vars():
     linux_json = '{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/g2/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION":"sqlite3://na:na@/tmp/sqlite/G2C.db"}}'
     # ANT linux_json = '{"PIPELINE": {"SUPPORTPATH": "/home/ant/senzprojs/3.8.0.23292/data", "CONFIGPATH": "/home/ant/senzprojs/3.8.0.23292/etc", "RESOURCEPATH": "/home/ant/senzprojs/3.8.0.23292/resources"}, "SQL": {"CONNECTION": "postgresql://senzing:password@ant76:5432:g2"}}'
     darwin_json = '{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/g2/resources","SUPPORTPATH":"/opt/senzing/g2/data"},"SQL":{"CONNECTION":"sqlite3://na:na@/tmp/sqlite/G2C.db"}}'
-    windows_json = r'{"PIPELINE":{"CONFIGPATH":"C:\Program Files\Senzing\g2","RESOURCEPATH":"C:\Program Files\Senzing\g2\resources","SUPPORTPATH":"C:\Program Files\Senzing\g2\data"},"SQL":{"CONNECTION":"sqlite3://na:na@C:\Temp\sqlite\G2C.db"}}'
+    windows_json = '{"PIPELINE":{"CONFIGPATH":"C:/Program Files/Senzing/g2","RESOURCEPATH":"C:/Program Files/Senzing/g2/resources","SUPPORTPATH":"C:/Program Files/Senzing/g2/data"},"SQL":{"CONNECTION":"sqlite3://na:na@C:/Temp/sqlite/G2C.db"}}'
 
     os = platform.system()
 
@@ -27,6 +27,6 @@ def engine_vars():
         eng_vars["ENGINE_CONFIGURATION_JSON"] = windows_json
 
     # Debug
-    print(eng_vars)
+    print(eng_vars["ENGINE_CONFIGURATION_JSON"])
 
     return eng_vars
