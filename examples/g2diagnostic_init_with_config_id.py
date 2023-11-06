7542ba929d65e714fba6f5dbe2439900a7a9e5cc
docktermj <michael@dockter.com>
2023-11-06 15:27:24 -0500

    #3 Savepoint

--- examples/g2diagnostic_init_with_config_id.py
+++ examples/g2diagnostic_init_with_config_id.py
@@ -1,4 +1,27 @@
-from senzing import g2diagnostic
+#! /usr/bin/env python3
 
-INI_PARAMS = '{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/g2/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION":"sqlite3://na:na@/tmp/sqlite/G2C.db"}}'
-G2_DIAGNOSTIC = g2diagnostic.G2Diagnostic("Example", INI_PARAMS, 0)
+import json
+
+from senzing import g2diagnostic
+from senzing.g2exception import G2Exception
+
+INI_PARAMS_DICT = {
+    "PIPELINE": {
+        "CONFIGPATH": "/etc/opt/senzing",
+        "RESOURCEPATH": "/opt/senzing/g2/resources",
+        "SUPPORTPATH": "/opt/senzing/data",
+    },
+    "SQL": {"CONNECTION": "sqlite3://na:na@/tmp/sqlite/G2C.db"},
+}
+MODULE_NAME = "Example"
+
+try:
+    G2_DIAGNOSIS = g2diagnostic.G2Diagnostic()
+
+    G2_DIAGNOSIS.init_with_config_id(MODULE_NAME, json.dumps(INI_PARAMS_DICT))
+
+    # Do work.
+
+    G2_DIAGNOSIS.destroy()
+except G2Exception as err:
+    print(err)
