40bddafdb3c8cf4e505752c3f33bcfbee006a51e
Ant <ant@senzing.com>
2023-11-06 14:01:20 -0500

    #3 - Fixes

--- examples/G2ConfigMgr/get_config_list.py
+++ examples/G2ConfigMgr/get_config_list.py
@@ -1,9 +1,8 @@
 from senzing import g2configmgr
 from senzing.g2exception import G2Exception
 
-# ENGINE_CONFIGURATION_JSON = '{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/g2/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION":"sqlite3://na:na@/tmp/sqlite/G2C.db"}}'
+ENGINE_CONFIGURATION_JSON = '{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/g2/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION":"sqlite3://na:na@/tmp/sqlite/G2C.db"}}'
 ENGINE_MODULE_NAME = "EXAMPLE"
-ENGINE_CONFIGURATION_JSON = '{"PIPELINE": {"CONFIGPATH": "/home/ant/senzprojs/3.8.0.23292/etc", "RESOURCEPATH": "/home/ant/senzprojs/3.8.0.23292/resources", "SUPPORTPATH": "/home/ant/senzprojs/3.8.0.23292/data"}, "SQL": {"CONNECTION": "postgresql://senzing:password@ant76:5432:g2"}}'
 ENGINE_VERBOSE_LOGGING = 0
 
 try:
