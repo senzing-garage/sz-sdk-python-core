b6ea5af132dd5c92c248a834db4d9951cdb13967
Ant <ant@senzing.com>
2024-05-07 17:43:16 -0400

    #3 Updates

---
+++ examples/extras/szdiagnostic/szdiagnostic_initialize_with_debug.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+
+from senzing import SzDiagnostic, SzError
+
+INSTANCE_NAME = "Example"
+SETTINGS = {
+    "PIPELINE": {
+        "CONFIGPATH": "/etc/opt/senzing",
+        "RESOURCEPATH": "/opt/senzing/g2/resources",
+        "SUPPORTPATH": "/opt/senzing/data",
+    },
+    "SQL": {"CONNECTION": "sqlite3://na:na@/tmp/sqlite/G2C.db"},
+}
+
+try:
+    sz_diagnostic = SzDiagnostic(INSTANCE_NAME, SETTINGS, verbose_logging=1)
+except SzError as err:
+    print(err)
