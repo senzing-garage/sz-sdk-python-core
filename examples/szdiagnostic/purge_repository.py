f418563c80c884fa86a02a7061caa8e52a0e839d
docktermj <michael@dockter.com>
2024-10-24 11:11:41 -0400

    #99 Savepoint

---
+++ examples/szdiagnostic/purge_repository.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+from senzing import SzAbstractFactory, SzAbstractFactoryParameters, SzError
+
+FACTORY_PARAMETERS: SzAbstractFactoryParameters = {
+    "instance_name": "Example",
+    "settings": {
+        "PIPELINE": {
+            "CONFIGPATH": "/etc/opt/senzing",
+            "RESOURCEPATH": "/opt/senzing/er/resources",
+            "SUPPORTPATH": "/opt/senzing/data",
+        },
+        "SQL": {"CONNECTION": "sqlite3://na:na@/tmp/sqlite/G2C.db"},
+    },
+}
+
+try:
+    sz_abstract_factory = SzAbstractFactory(**FACTORY_PARAMETERS)
+    sz_diagnostic = sz_abstract_factory.create_sz_diagnostic()
+    # WARNING
+    # WARNING - This will remove all loaded and entity resolved data from the Senzing repository, use with caution!
+    # WARNING
+    sz_diagnostic.purge_repository()
+except SzError as err:
+    print(f"\nError: {err}\n")
