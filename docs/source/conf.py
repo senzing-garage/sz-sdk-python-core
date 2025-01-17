320f930cbd605b785a205e02041c9cc5556fc991
Ant <ant@senzing.com>
2025-01-17 10:49:22 -0500

    #153 - Test workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,22 +34,6 @@ html_static_path = ["_static"]
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
-# TODO
-GIT_ACTIONS = os.getenv("GITHUB_ACTIONS", "")
-if not GIT_ACTIONS:
-    print("Not running in a Github action...")
-    git_branch_name = subprocess.run(
-        ["git", "symbolic-ref", "--short", "HEAD"], capture_output=True, check=True
-    ).stdout.decode(encoding="utf-8")
-else:
-    print("Running in a Github action...")
-    git_branch_name = os.getenv("GITHUB_REF_NAME", "")
-git_branch_name = git_branch_name.strip()
-print(f"\n{git_branch_name = }\n")
-
-envs = os.environ
-for k, v in envs.items():
-    print(f"{k}={v}")
 
 extensions = [
     "autodocsumm",  # to generate tables of functions, attributes, methods, etc.
@@ -69,26 +53,63 @@ html_theme = "sphinx_rtd_theme"
 # autodoc_inherit_docstrings = False  # don't include docstrings from the parent class
 # autodoc_typehints = "description"   # Show types only in descriptions, not in signatures
 
+# TODO
+PROCESS_DOCSTRING_DEBUG = os.getenv("SPHINX_PROCESS_DOCSTRING_DEBUG", "")
+# TODO
+try:
+    GIT_ACTIONS = os.getenv("GITHUB_ACTIONS", "")
+    if not GIT_ACTIONS:
+        print("PROCESS_DOCSTRING: Not running in a Github action...")
+
+        # Example response = 153-ant-1
+        git_branch = subprocess.run(
+            ["git", "symbolic-ref", "--short", "HEAD"], capture_output=True, check=True
+        ).stdout.decode(encoding="utf-8")
+
+        # Example response = git@github.com:senzing-garage/sz-sdk-python-core.git
+        git_repo = subprocess.run(
+            ["git", "config", "--get-all", "remote.origin.url"], capture_output=True, check=True
+        ).stdout.decode(encoding="utf-8")
+        if git_repo:
+            git_repo_list = git_repo.split(":")
+            git_repo = git_repo_list[-1]
+            git_repo = git_repo.replace(".git", "")
+    else:
+        print("PROCESS_DOCSTRING: Running in a Github action...")
+        # Example response = 153-ant-1
+        git_branch = os.getenv("GITHUB_REF_NAME", "")
+        # Example response = senzing-garage/sz-sdk-python-core
+        git_repo = os.getenv("GITHUB_REPOSITORY", "")
+
+    git_branch = git_branch.strip()
+    git_repo = git_repo.strip()
+
+    # TODO Debug
+    print(f"PROCESS_DOCSTRING: {git_branch = }")
+    print(f"PROCESS_DOCSTRING: {git_repo = }\n")
+except (subprocess.CalledProcessError, FileNotFoundError, TypeError, IndexError) as err:
+    print(f"\nERROR: Failed trying to process doc strings - {err}\n")
+    sys.exit(1)
+
 
 # TODO
 def process_docstring(app, what, name, obj, options, lines):
     # print("\nDEBUG: In process_docstring()...\b")
 
-    if "add_record" in name:
+    if PROCESS_DOCSTRING_DEBUG:
         print(f"\n{app = }")
         print(f"{what = }")
         print(f"{name = }")
         print(f"{obj = }")
         print(f"{options = }")
         print(f"{lines = }\n")
-        for line in lines:
-            print(f"\t{line}")
 
     for i, line in enumerate(lines):
-        # line: str = line.strip()
-        if ".. rli:: https://raw.githubusercontent.com/senzing-garage/sz-sdk-python-core/refs/heads/" in line:
-            print(f"Replacing for sz-sdk-python-core in: {line}")
-            lines[i] = line.replace("/main/", f"/{git_branch_name}/")
+        # .. rli:: https://raw.githubusercontent.com/senzing-garage/sz-sdk-python-core/refs/heads/main/examples/szengine/add_record.py
+        if f".. rli:: https://raw.githubusercontent.com/{git_repo}/refs/heads/main/examples/" in line:
+            print(f"PROCESS_DOCSTRING: Replacing /main/ with /{git_branch}/ for {what} {name}, line: {line.strip()}")
+            lines[i] = line.replace("/main/", f"/{git_branch}/")
+            print(f"\tPROCESS_DOCSTRING: {lines[i].strip()}\n")
 
 
 def setup(app):
