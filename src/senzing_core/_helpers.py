1283aeac02f3491d8ef57b2faa429c6af55a5430
Ant <ant@senzing.com>
2025-06-10 17:21:21 -0400

    #269 - Type fix

--- src/senzing_core/_helpers.py
+++ src/senzing_core/_helpers.py
@@ -33,10 +33,10 @@ from typing import cast as typing_cast
 from senzing import ENGINE_EXCEPTION_MAP, SzError, SzSdkError
 
 try:
-    import orjson   # type: ignore
+    import orjson  # type: ignore[import-not-found]
 
     def _json_dumps(object_: Any) -> str:
-        return orjson.dumps(object_).decode("utf-8") 
+        return orjson.dumps(object_).decode("utf-8")  # type: ignore
 
 except ImportError:
     import json
